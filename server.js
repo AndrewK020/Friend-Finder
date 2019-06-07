b22fde5dd375a516368ba98f7a0c0fe1e7819e64
Andrew Kelly <drew12396@gmail.com>
2019-06-07 17:51:41 -0500

    added dynamic port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
