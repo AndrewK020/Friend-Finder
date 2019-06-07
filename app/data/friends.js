d6e1502f5e2c9237c112978b5ca0d6484694c62f
Andrew Kelly <drew12396@gmail.com>
2019-06-07 17:40:07 -0500

    added working matching functionality

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,7 +14,39 @@ var friends = [
             4,
             1
           ]
-    },     
+    }, 
+    {
+      name: "Mr Rabbit",
+      photo: "https://en.wikipedia.org/wiki/Rabbit#/media/File:Oryctolagus_cuniculus_Rcdo.jpg",
+      scores: [
+        5,
+        5,
+        1,
+        1,
+        1,
+        5,
+        1,
+        1,
+        1,
+        5
+      ]
+    },
+    {
+      name: "Average Guy",
+      photo: "https://2.bp.blogspot.com/-C1lKVFS5wHs/T3IW1ZBgOAI/AAAAAAAAAHA/cvvEYtNVxnc/s1600/nice-guy-dating.jpg",
+      scores: [
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3
+      ]
+    }    
 ];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,9 +7,29 @@ module.exports = function(app) {
     });
 
     app.post('/api/friends', function(req, res) {
-        console.log(req.body);
+
+        // finds smallest number in array 
+        Array.min = function( array ){
+            return Math.min.apply( Math, array );
+        };
+
         var newFriend = req.body;
 
+        // find the difference between the new friend and the friends list
+        var matches = [];
+        const count = friends.length;
+        for(var i = 0; i < count; i++) {
+            var totalDifference = 0;
+            for(var j = 0; j < friends[i].scores.length; j++) {
+                totalDifference += Math.abs(newFriend.scores[j] - friends[i].scores[j]);
+            }
+            matches.push(totalDifference);
+        }
+        var smallestDiff = Array.min(matches);
+        
+        var match = friends[matches.indexOf(smallestDiff)];
+
         friends.push(newFriend);
+        res.json(match);
     });
 };
